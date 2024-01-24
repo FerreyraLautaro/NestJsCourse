c22b2b899e54747dd33c38a3e126359f33638540
FerreyraLautaro <ferreyralautaro69@gmail.com>
2024-01-24 16:40:57 -0300

    classes 3.1

--- typescript-vite/src/bases/03-calsses.ts
+++ typescript-vite/src/bases/03-calsses.ts
@@ -8,7 +8,25 @@
 //         console.log("constructor llamado");
 //       }
 // }
-export class Pokemon {//forma corta de creacion de clase
-  constructor(public readonly id: number, public name: string) {}
+export class Pokemon {
+  //forma corta de creacion de clase
+
+  get imageUrl():string{
+    return `https://pokemon.com/${this.id}`
+  }
+  constructor(
+    public readonly id: number,
+    public name: string,
+  ) {}
+
+    scream () {
+      console.log(`${this.name.toUpperCase()} !!!`);
+    }
+    speak () {
+      console.log(`${this.name} , this.name!`);
+    }
 }
-export const charmander = new Pokemon(1,"Charmander")
+export const charmander = new Pokemon(
+  1,
+  "Charmander",
+);
